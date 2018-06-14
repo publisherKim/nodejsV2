3a1c6e4f8d6cc736c8b4a61824c431af7a5d9c6f
es-jukim <es-jukim@smilegate.com>
2018-06-14 13:17:17 +0900

    37

---
+++ 037-CORELevel1Exam/concept.js
@@ -0,0 +1,17 @@
+/*
+    zsls: Alice, Wang. Alice bought a tea from Wang at Bejing.
+    the price of the tea is 10 yuan.
+    Write an exchange program which calculate the price in the fiat currency of the payer.
+    Pay the 10 yuan from Alice's balance to Wang's balance.
+    Rewrite the bankAccounts.
+
+    기대값: "the price ${10} ${unit} was paid from Alice's account balance ${credit} to Wang's balance ${resultCredit}. After the transaction, the balance of Alice is ${Alice}, and that of Wang is ${Wang}"
+    crud + 함수형 코딩
+    
+    설계
+    structure
+        data/data.js (자료)
+    아무 생각이 없다.
+    일단 파일 로드해서 콘솔부터 찍어보자.
+    program.js 라고 이름짓고 쪼갤수 있으면 쪼개고 아니면 쪼개지 말자.    
+*/
---
+++ 038-CORELevel2Exam/app.js
@@ -0,0 +1,3 @@
+const crud = require('./json-exercies/crud');
+const accountList = crud.fetchList();
+console.log(accountList);
